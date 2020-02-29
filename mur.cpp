98c4e530fce955c5234b68df437945246ccf35b2
Dorian <61635324+dorian-gabIer@users.noreply.github.com>
2020-02-29 22:11:45 +0100

    Create mur.cpp

---
+++ mur.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, l;
+    cin >> a >> b >> l;
+    int c = ((a * 10 + b * 20) / l);
+    cout << (c*10) << endl;
+    return 0;
+}
