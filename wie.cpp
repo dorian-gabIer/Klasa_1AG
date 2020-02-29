ece5f6a0015b127d20b73e2591ba1aafabb26643
Dorian <61635324+dorian-gabIer@users.noreply.github.com>
2020-02-29 22:44:10 +0100

    63/100 points

---
+++ wie.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr);
+    int n, m, i, a, temp, cstair = 0;
+    vector<int>stairs;
+    bool itdid = false;
+    cin >> n >> m;
+    for(i = 0; i < n; i++)
+    {
+        cin >> temp;
+        stairs.push_back(temp);
+    }
+    for(i = 0; i < m; i++)
+    {
+        cin >> temp;
+        for(a = 0; a < n; a++)
+        {
+           if(!itdid){
+               if(stairs[a] < temp)
+               {
+                    cstair++;
+               }
+               else
+               {
+                    cout << cstair << " ";
+                    itdid = true;
+               }
+        }
+        }
+        if(!itdid)
+        {
+            cout << cstair << " ";
+        }
+        cstair = 0;
+        itdid = false;
+    }
+}
