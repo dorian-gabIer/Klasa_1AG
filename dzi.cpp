55e05e04a1fde66059cf84a3b86fbe5503e39a52
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-03-01 23:22:42 +0100

    71/100 points

--- dzi.cpp
+++ dzi.cpp
@@ -1,57 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n, k, g = 0, b = 0;
-    cin >> n >> k;
-    int minboys = n;
-    int a[n];
-    for (auto &val : a)
-    {
-        cin >> val;
-        if (val == 0)
-        {
-            g++;
-        }
-    }
-    if (g < k)
-    {
-        cout << "NIE";
-    }
-    else if (g == k)
-    {
-        cout << n - k;
-    }
-    else
-    {
-        for (int i = 0; i < n - k; i++)
-        {
-            if (a[i] == 1)
-            {
-                continue;
-            }
-            g = 0;
-            for (int c = 0; c < n - i; c++)
-            { //for every position after that
-                if (a[i + c] == 0)
-                {
-                    g++;
-                    if (g == k)
-                    {
-                        minboys = min(minboys, b);
-                        b = 0;
-                        break;
-                    }
-                }
-                else
-                {
-                    b++;
-                }
-            }
-        }
-    }
-    cout << minboys;
-    return 0;
+int main() {
+	// just in case
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int* bg = NULL; // boys and girls
+	int n = 0, k = 0; // n: amount of people, k: wanted girls with no boys between them
+	int g = 0; //girls
+	int gt, minb, aob; // gt: girls together  minb: min boys to go aob: actual amount of boys
+	int jj;
+	vector<int>gp; // vector with place in the row for each girl
+
+	cin >> n >> k;
+	bg = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> bg[i];
+		if (bg[i] == 0)
+		{
+			g++;
+			gp.push_back(i);
+		}
+	}
+	if (g < k)
+	{ // not enough girls 
+		cout << "NIE" << endl;
+		return 0;
+	}
+
+	if (g == 1)
+	{ // only place for one girl is asked, no boy has to leave the line
+		cout << "0" << endl;
+		return 0;
+	}
+
+	minb = n - g; // worst case
+	aob = 0;
+	gt = 1;
+	for (int j = 0; j < g; j++)
+	{
+		if (g - j < k)
+			break;
+		jj = 1;
+		gt = 1;
+		while (gt < k)
+		{
+			while (gp[j] + jj < n )
+			{
+				if (bg[gp[j] + jj] == 0)
+				{
+					gt++;
+					if (gt == k)
+						break;
+				}
+				else
+				{
+					aob++;
+				}
+				jj++;
+			}
+		}
+		if (aob < minb)
+		{
+			minb = aob;
+		}
+		aob = 0;
+	}
+	cout << minb << endl;
+	return 0;
 }
