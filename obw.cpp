8006a19c370398e9e709c2767e2f7aee66985cd7
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-03-01 21:07:58 +0100

    Update obw.cpp

--- obw.cpp
+++ obw.cpp
@@ -1,26 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int n, i, tmpw, tmpb, sum = 0, c = 0;
-    vector<int>cable;
-    cin >> n;
-    for(i = 0; i <= n-1; i++)
-    {
-        cin >> tmpw;
-        cin >> tmpb;
-	cable.push_back(tmpw-tmpb);
+struct kabel{
+    kabel(int w, int n){
+        wyt = w;
+        nap = n;
+        ratio = wyt-nap;
     }
-    sort(cable.begin(),cable.end());
-    for(i = n-1; i >= 0; i--)
-    {
- 	sum = sum +  cable[i];
- 	if (sum < 0)
-        {
- 		break;
+    int wyt;
+    int nap;
+    int ratio = 0;
+};
+bool my_sort(const kabel &a, const kabel &b){
+    return a.ratio > b.ratio;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    int n; cin >> n;
+    int ik = 0;
+    long long suma = 0;
+    vector<kabel> kable;
+    for(int i = 0; i < n; ++i){
+        int a, b;
+        cin >> a >> b;
+
+        if(a >= b){ 
+            suma += (a-b);
+            ik++;
         }
- 	c++;
-    }    
-    cout << c;
+        else
+            kable.push_back(kabel(a, b));
+    }
+    sort(kable.begin(), kable.end(), my_sort);
+    for(kabel k: kable){
+        if(suma + k.ratio >= 0){
+            suma += k.ratio;
+            ik++;
+        }
+        else break;
+    }
+    printf("%i", ik);
     return 0;
 }
