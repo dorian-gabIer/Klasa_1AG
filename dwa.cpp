93d7585bfd2501abae10d374ff78e94ba59e64f7
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-03-08 00:31:03 +0100

    66/100 points

--- dwa.cpp
+++ dwa.cpp
@@ -1,40 +1,38 @@
-//DwaSlowa
 #include <iostream>
 #include <string>
 using namespace std;
-
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
 	int n, t, a, b;
 	string str1, str2;
-	char temp[1];
-
+	char temp;
 	cin >> n;
 	cin >> str1;
 	cin >> str2;
 	cin >> t;
-
 	for (int i = 0; i < t; i++)
 	{
 		cin >> a >> b;
-		temp[0] = str2[b];
+		temp = str2[b];
 		str2[b] = str1[a];
-		str1[a] = temp[0];
-
+		str1[a] = temp;
 		if (str1 == str2) {
-			cout << "0" << endl;
+			cout << "0" << "\n";
+             continue;
 		}
-		else
+        if (str1 > str2)
 		{
-			if (str1 > str2)
-			{
-				cout << "1" << endl;
-			}
-			else
-			{
-				cout << "2" << endl;
-			}
+			cout << "1" << "\n";
+             continue;
 		}
+        if (str1 < str2)
+		{
+			cout << "2" << "\n";
+             continue;
+		}
+        continue;
 	}
 	return 0;
 }
