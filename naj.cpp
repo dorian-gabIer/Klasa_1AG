59483069896271ac50aaafb379dbd8f92217b832
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-03-10 19:52:56 +0100

    Create naj.cpp

---
+++ naj.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std; 
+void printLCSubStr(char* X, char* Y, int m, int n) 
+{ 
+    int LCSuff[m + 1][n + 1]; 
+    int len = 0; 
+    int row, col; 
+    for (int i = 0; i <= m; i++) { 
+        for (int j = 0; j <= n; j++) { 
+            if (i == 0 || j == 0) 
+                LCSuff[i][j] = 0; 
+  
+            else if (X[i - 1] == Y[j - 1]) { 
+                LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1; 
+                if (len < LCSuff[i][j]) { 
+                    len = LCSuff[i][j]; 
+                    row = i; 
+                    col = j; 
+                } 
+            } 
+            else
+                LCSuff[i][j] = 0; 
+        } 
+    } 
+    if (len == 0) { 
+        cout << "0"; 
+        return; 
+    }
+    char* resultStr = (char*)malloc((len + 1) * sizeof(char)); 
+    while (LCSuff[row][col] != 0) { 
+        resultStr[--len] = X[row - 1];
+        row--; 
+        col--; 
+    }
+    size_t lenwef = strlen(resultStr);
+    cout << lenwef; 
+}
+int main() 
+{
+    char X[101];
+    char Y[101];
+    cin >> X;
+    cin >> Y;
+    int m = strlen(X); 
+    int n = strlen(Y); 
+    printLCSubStr(X, Y, m, n);
+    return 0;
+}
