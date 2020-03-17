79d3b3acb7cccfaafa35cf301391497d906f4af9
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-03-17 17:51:44 +0100

    Create kol.cpp

---
+++ kol.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int r;
+    double pole;
+    cin >> r;
+    pole = (3.14 * r * r);
+    cout << fixed << setprecision(2) << pole;
+    return 0;
+}
