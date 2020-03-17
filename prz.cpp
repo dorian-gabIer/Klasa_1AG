c58dcb5ee845935ce5f8ad3c1a1dc58ae221060d
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-03-17 17:58:05 +0100

    Create prz.cpp

---
+++ prz.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    double dg = sqrt((a*a) + (b*b));
+    cout << fixed << setprecision(2) << dg;
+    return 0;
+}
