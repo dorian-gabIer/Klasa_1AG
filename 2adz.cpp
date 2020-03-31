e6848e77e7bd6799121af85bcfe956895905144b
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-03-31 15:43:33 +0200

    Create 2adz.cpp

---
+++ 2adz.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int temp, c = 0;
+    while(cin >> temp)
+    {
+        c++;
+        cout << c << endl;
+    }
+    return 0;
+}
