4b06fe9a73b5a0a868bf0ef8b5fde350e6fb01e0
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-03-31 15:44:04 +0200

    Create 5sum.cpp

---
+++ 5sum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int temp, sum = 0;
+    while(cin >> temp)
+    {
+        if(temp == 0) break;
+        sum = (sum) + (temp);
+    }
+    cout << sum;
+    return 0;
+}
