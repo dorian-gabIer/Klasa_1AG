b9e07a2940dbdc0f8df07931331e173135fa7650
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-04-29 11:06:52 +0200

    Create 1wit.cpp

---
+++ 1wit.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string in;
+    cin >> in;
+    cout << "Witaj " << in << endl;
+    if(in.back() == 'a') cout << "Witam Szanowna Pania";
+    else cout << "Witam Szanownego Pana";
+    return 0;
+}
