3d64c73268fb0120b6bdb9225e9f07b6ae1eb0b4
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-05-18 12:50:50 +0200

    Update 1d2a.cpp

--- 1d2a.cpp
+++ 1d2a.cpp
@@ -2,46 +2,34 @@
 using namespace std;
 string dec2all(int x, int s)
 {
+    int i = 0, temp;
+    int numbs[15000];
     string res;
-    while(1)
+    while(x != 0)
     {
-        if(x==0) break;
-        if(x%s == 0)
-        {
-            res = '0' + res;
-            x /= s;
-        }
-        if((x-1)%s == 0)
-        {
-            res = '1' + res;
-            x -= 1;
-            x /= s;
-        }
-        if((x-2)%s == 0)
-        {
-            res = '2' + res;
-            x -= 2;
-            x /= s;
-        }
-        if((x-3)%s == 0)
-        {
-            res = '3' + res;
-            x -= 3;
-            x /= s;
-        }
-        if((x-4)%s == 0)
-        {
-            res = '4' + res;
-            x -= 4;
-            x /= s;
-        }
+        temp = x % s;
+        x /= s;
+        numbs[i] = temp;
+        i++;
+        temp = 0;
+    }
+    for(int z = i - 1; z >= 0; z--)
+    {
+        if(numbs[z] == 10) res += "A";
+        else if(numbs[z] == 11) res += "B";
+        else if(numbs[z] == 12) res += "C";
+        else if(numbs[z] == 13) res += "D";
+        else if(numbs[z] == 14) res += "E";
+        else if(numbs[z] == 15) res += "F";
+        else res += to_string(numbs[z]);
     }
     return res;
 }
-int main()
+int main() 
 {
-    int x, s;
-    cin >> s >> x;
-    cout << dec2all(x, s);
-    return 0;
+  ios_base::sync_with_stdio(0);
+  int s, x;
+  cin >> s >> x;
+  cout << dec2all(x, s);
+  return 0;
 }
