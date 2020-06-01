aa8e647a1a73c2b1a6ee6bb60199fb32af6d71a0
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-06-01 22:36:30 +0200

    Update 1u11.cpp

--- 1u11.cpp
+++ 1u11.cpp
@@ -1,56 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-string dec2bin(int n)
-{
-    string res;
-    while(1)
+int main() {
+  int n, x, a;
+  cin>>n>>x;
+  string bin;
+  a=x;
+  while(x!=0)
+  {
+    if(x%2==0)
     {
-        if(n == 0) break;
-        if(n%2 == 0)
-        {
-            res += "0";
-            n /= 2;
-        }
-        else
-        {
-            res += "1";
-            n-= 1;
-            n /= 2;
-        }
+      bin='0'+bin;
     }
-    reverse(res.begin(), res.end());
-    return res;
-}
-string dec2u1(int i, int n)
-{
-    i *= 8;
-    string res, bin, res2;
-    if(n>=0)
+    else
     {
-        bin = dec2bin(n);
-        res = bin;
-        while (res.size() < i) res = '0' + res;
-        if(res.size() > i) return "ERROR";
-        return res;
+      bin = '1' + bin;
     }
-    else {
-        bin = dec2bin(-n);
-        res = bin;
-        while (res.size() < i) res = '0' + res;
-        if(res.size() > i) return "ERROR";
-        for(auto x : res)
+    x /= 2;
+  }
+  if(bin.size()>=n*8)
+  {
+    cout<<"ERROR";
+  }
+  else
+  {
+    while(bin.size()<n*8)
+    {
+      bin='0'+bin;
+    }
+    if(a<0)
+    {
+        for(int i=0; i<bin.size(); i++)
         {
-            if(x == '1') res2 += '0';
-            if(x == '0') res2 += '1';
+            if(bin[i]=='0')
+            {
+                bin[i]='1';
+            }
+            else
+            {
+                bin[i]='0';
+            }
         }
-        return res2;
     }
-}
-int main()
-{
-    int i, n;
-    cin >> i >> n;
-    string res = dec2u1(i, n);
-    cout << res;
-    return 0;
+    cout << bin;
+  }
 }
